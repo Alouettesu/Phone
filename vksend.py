b94a9d1248860c580b5df27e9109553e88036627
Alouettesu <sdianoff@mail.ru>
2023-09-08 22:08:35 +0500

    Отправлять все сообщения, а не только непрочитанные

--- vksend.py
+++ vksend.py
@@ -9,17 +9,16 @@ __vk = __vk_session.get_api()
 def send_sms(smsList):
     try:
         for sms in smsList.smsList:
-            if sms.Stat == 'REC UNREAD':
-                __vk.messages.send(
-                    user_id = config.vk_user_id,
-                    message = 'Новое сообщение от ' + sms.Sender,
-                    random_id = random.randint(1, 2**31)
-                )
-                __vk.messages.send(
-                    user_id = config.vk_user_id,
-                    message = sms.Text,
-                    random_id = random.randint(1, 2**31)
-                )
+            __vk.messages.send(
+                user_id = config.vk_user_id,
+                message = 'Новое сообщение от ' + sms.Sender,
+                random_id = random.randint(1, 2**31)
+            )
+            __vk.messages.send(
+                user_id = config.vk_user_id,
+                message = sms.Text,
+                random_id = random.randint(1, 2**31)
+            )
     except:
         return False
     return True
